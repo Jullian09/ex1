a36a1205e9ceaebbe40d6112fd60a57727a47717
Jullian <jullianoliver@gmail.com>
2018-01-25 18:12:51 +0100

    Les utilisateurs peuvent se connecter (si ils ont juste)

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,14 +2,31 @@ class UsersController < ApplicationController
   def index
   end
 
+#L'inscription des utilisateurs
+
   def new
-    @user=User.new
+    @user = User.new
   end
 
   def create
-    @user= User.create (user_params)
+    @user = User.create (user_params)
     if @user.save
       redirect_to root_path, notice: "Bienvenue à toi, nouvel utilisateur"
+    end
+  end
+
+#La connexion des utilisateurs
+
+  def login
+    @user = User.new
+  end
+
+  def check
+    @current_user = User.where(user_params).first
+    if @current_user
+      redirect_to root_path, notice: "Bienvenue à toi, #{@current_user.name} !"
+    else
+      redirect_to user_login_path, alert: "Vous avez dû vous tromper dans le mot de passe ou l'identifiant, veuillez recommencer"
     end
   end
 
--- config/routes.rb
+++ config/routes.rb
@@ -3,9 +3,14 @@ Rails.application.routes.draw do
   root 'advertisements#index'
 
   get '/users/index', to: 'users#index'
+
+  #Pour l'inscription de l'utilisateur
   get '/user/new', to: 'users#new', as: 'user_new'
   post '/user/new', to: 'users#create'
+
+  #Pour la connextion de l'utilisateur
   get '/user/login', to: 'users#login', as: 'user_login'
+  post '/user/login', to: 'users#check'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
